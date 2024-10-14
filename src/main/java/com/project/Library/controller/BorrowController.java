967a7cf12ae902e1dced0d49ff5d81df0993017c
Ahmed Tarek (オリ ー オ) <83515041+Oz174@users.noreply.github.com>
2024-10-14 12:09:24 +0300

    just a minor check

--- src/main/java/com/project/Library/controller/BorrowController.java
+++ src/main/java/com/project/Library/controller/BorrowController.java
@@ -17,12 +17,12 @@ public class BorrowController {
     private BorrowService borrowService;
 
     @PostMapping("/{book_id}/borrow/{patron_id}")
-    public String borrowBook(@PathVariable int book_id , @PathVariable int patron_id , @RequestedBody BorrowBookDTO borrowBookDTO){
+    public String borrowBook(@PathVariable(value="book_id") int book_id , @PathVariable(value="patron_id") patron_id , @RequestedBody BorrowBookDTO borrowBookDTO){
         return borrowService.borrowBook(book_id,patron_id,borrowBookDTO);
     }
 
     @PostMapping("/{book_id}/return/{patron_id}")
-    public String returnBook(@PathVariable int book_id , @PathVariable int patron_id){
+    public String returnBook(@PathVariable(value="book_id") int book_id , @PathVariable(value="patron_id") patron_id ){
         return borrowService.returnBook(book_id,patron_id);
     }
 
